505fce8e520f3bad1846904c017c61003b8a674b
jiho Shin <jhhj424@naver.com>
2021-09-10 23:59:44 +0900

    TIS(LeetCode): DivideTwoIntegers
    https://leetcode.com/problems/divide-two-integers/

---
+++ src/leetcode/_0029DivideTwoIntegers.java
@@ -0,0 +1,33 @@
+package leetcode;
+
+
+public class _0029DivideTwoIntegers {
+    public int divide(int dividend, int divisor) {
+        if (divisor == 0) return Integer.MAX_VALUE;
+        if (dividend == 0) return 0;
+        long result = helper(dividend, divisor);
+        return result > Integer.MAX_VALUE ? Integer.MAX_VALUE : (int) result;
+    }
+
+    public long helper(long dividend, long divisor) {
+        boolean isNegative = dividend < 0 != divisor < 0;
+        dividend = Math.abs(dividend);
+        divisor = Math.abs(divisor);
+        if (dividend < divisor) return 0;
+
+        long res = 0;
+        while (dividend >= divisor) {
+            long t = divisor;
+            long p = 1;
+            long tt = t << 1;
+            while (dividend > tt) {
+                t = tt;
+                p <<= 1;
+                tt <<= 1;
+            }
+            res += p;
+            dividend -= t;
+        }
+        return isNegative ? -res : res;
+    }
+}
