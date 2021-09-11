20836d9302c2ad5cb948275d397ada5db438fbd1
Jiho Shin <46177529+jhhj424@users.noreply.github.com>
2021-09-11 23:39:29 +0900

    TIS(LeetCode): SearchInsertPosition
    https://leetcode.com/problems/search-insert-position/

---
+++ src/leetcode/_0035SearchInsertPosition.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+
+public class _0035SearchInsertPosition {
+    public int searchInsert(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] >= target) {
+                return i;
+            }
+        }
+        return nums.length;
+    }
+}
