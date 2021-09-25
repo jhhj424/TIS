787df32c723d8139b32bc849be106558493324e6
zito-s <jhhj424@naver.com>
2021-09-25 22:29:11 +0900

    TIS(LeetCode): SameTree
    https://leetcode.com/problems/same-tree/

---
+++ src/leetcode/_0100SameTree.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+
+public class _0100SameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null || p.val != q.val) return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
