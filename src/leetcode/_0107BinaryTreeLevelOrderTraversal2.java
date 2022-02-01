ee476b3451b13510daa97e602b44452320594183
zito-s <jhhj424@naver.com>
2022-02-01 23:44:37 +0900

    TIS(LeetCode): 107. Binary Tree Level Order Traversal II
    https://leetcode.com/problems/binary-tree-level-order-traversal-ii/

---
+++ src/leetcode/_0107BinaryTreeLevelOrderTraversal2.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+public class _0107BinaryTreeLevelOrderTraversal2 {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return Math.max(left, right) + 1;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
